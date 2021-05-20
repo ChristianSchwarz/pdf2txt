86564dacb52ec4990f500da871c2d13c780f8763
Christian Schwarz <christian.schwarz@iav.de>
2021-05-20 13:51:09 +0200

    added djvu support

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,8 @@
+//import com.aspose.imaging.Image
+//import com.aspose.imaging.fileformats.djvu.DjvuImage
+import com.lizardtech.djvu.DjVuPage
+import com.lizardtech.djvu.Document
+import com.lizardtech.djvubean.DjVuImage
 import kotlinx.coroutines.*
 import net.sourceforge.tess4j.ITessAPI
 import net.sourceforge.tess4j.Tesseract
@@ -6,15 +11,26 @@ import org.apache.pdfbox.pdmodel.PDDocument
 import org.apache.pdfbox.rendering.ImageType
 import org.apache.pdfbox.rendering.PDFRenderer
 import org.slf4j.LoggerFactory
+import java.awt.Canvas
+import java.awt.Graphics
+import java.awt.Image
 import java.awt.image.BufferedImage
 import java.io.File
+import java.io.FileWriter
 import java.io.IOException
+import java.net.URL
+import java.nio.file.Path
 import java.util.concurrent.ConcurrentSkipListMap
 import javax.imageio.ImageIO
+import kotlin.io.path.ExperimentalPathApi
+import kotlin.io.path.Path
+import kotlin.io.path.extension
+
 
 private val logger = LoggerFactory.getLogger(LoggHelper().toString())
 
-const val INPUT_PDF = """.\data\11437.pdf"""
+//const val INPUT_FILE = """.\data\11437.pdf"""
+const val INPUT_FILE = """.\data\Бабич - Наши авианосцы на стапелях и в дальних походах - 2003.djvu"""
 
 fun main() = runBlocking {
     print(File(".").absolutePath)
@@ -29,15 +45,21 @@ fun main() = runBlocking {
         }
     }
 
-    deferred.forEach { it.await() }
+    deferred.awaitAll()
 
 
-    pages.forEach { (page: Int, text: String) ->
-        println(
-            """-----------------------------------  $page  ------------------------------------
+    FileWriter("$INPUT_FILE.txt").use {
+        pages.forEach { (page: Int, text: String) ->
+
+            val message = """-----------------------------------  $page  ------------------------------------
             $text""".trimMargin()
-        )
+            println(
+                message
+            )
+            it.append(message)
+        }
     }
+
 
 }
 
@@ -57,26 +79,32 @@ private fun doOCR(pageImage: BufferedImage): String {
 
 }
 
+@OptIn(ExperimentalPathApi::class, kotlin.ExperimentalStdlibApi::class)
 private suspend fun loadPages(onPageImageLoaded: PageImageLoaded) {
 
 
-    val cacheFolder = File("""$INPUT_PDF.img\""")
+    val cacheFolder = File("""$INPUT_FILE.img\""")
     if (cacheFolder.isDirectory) {
         loadCachedPages(cacheFolder, onPageImageLoaded)
         return
     }
 
     cacheFolder.mkdirs()
-
-    convertPdfToBufferedImages(File(INPUT_PDF)) { img, index ->
+    val onImageExtracted: PageImageLoaded = { img, index ->
         onPageImageLoaded(img, index)
         val file = File(cacheFolder, "$index.png")
         ImageIO.write(img, "png", file)
+    }
+    val inputFile = Path(INPUT_FILE)
 
-
+    when (inputFile.extension.lowercase()) {
+        "pdf" -> convertPdfToBufferedImages(File(INPUT_FILE), onImageExtracted)
+        "djvu" -> convertDjvuToBufferedImages(inputFile, onImageExtracted)
     }
 
+
 }
+
 
 private fun loadCachedPages(cacheFolder: File, onPageImageLoaded: PageImageLoaded) {
     var pageIndex = 0
@@ -98,7 +126,63 @@ private fun loadCachedPages(cacheFolder: File, onPageImageLoaded: PageImageLoade
 
     }
 }
+//
+//@OptIn(ExperimentalPathApi::class)
+//private suspend fun convertDjvuToBufferedImages(inputFile: Path, onImageExtracted: PageImageLoaded) {
+//    val doc = Image.load(inputFile.absolutePathString()) as DjvuImage
+//
+//    File("$INPUT_FILE.img/").mkdirs()
+//    for (page in 0..doc.pageCount) {
+//
+//        val djvuPage = doc.djvuPages[page]
+//
+//
+//        //val bufferedImage = djvuPage.backgroundImage.toBitmap() // Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 3074958 out of bounds for length 3074958
+//        //val bufferedImage = djvuPage.image.toBitmap() //only images, text is missing
+//
+//        //val bufferedImage = djvuPage.foregroundImage.toBitmap() //java.lang.ArrayIndexOutOfBoundsException: Index 193116 out of bounds for length 193116
+//        // val bufferedImage = djvuPage.textImage.toBitmap() //only copyright disclaimer visible in the top left corner
+//        val bounds = djvuPage.bounds
+//        val pixelArray: IntArray = djvuPage.loadArgb32Pixels(bounds)
+//        val bufferedImage=BufferedImage(djvuPage.width,djvuPage.height, BufferedImage.TYPE_INT_RGB)
+//
+//        pixelArray.forEachIndexed { index, pixel ->
+//            val x:Int= index % bounds.width
+//            val y:Int = index / bounds.width
+//            bufferedImage.setRGB(x,y,pixel)
+//        }
+//
+//
+//
+//        onImageExtracted(bufferedImage, page)
+//    }
+//}
 
+
+
+@OptIn(ExperimentalPathApi::class)
+private suspend fun convertDjvuToBufferedImages(inputFile: Path, onImageExtracted: PageImageLoaded) {
+    val document = Document()
+    document.setAsync(true)
+    document.read(inputFile.toUri().toURL())
+
+
+
+    val canvas = Canvas()
+    for (index in 0 until document.size()) {
+
+        val pages = arrayOf<DjVuPage>(document.getPage(index, DjVuPage.MAX_PRIORITY, true))
+        val djvuImage = DjVuImage(pages, true)
+        val image_local: Image = djvuImage.getImage(canvas, djvuImage.getPageBounds(0))[0]
+
+        val image = BufferedImage(image_local.getWidth(null), image_local.getHeight(null), BufferedImage.TYPE_INT_ARGB)
+
+        val g: Graphics = image.createGraphics()
+        g.drawImage(image_local, 0, 0, null)
+        g.dispose()
+        onImageExtracted(image, index)
+    }
+}
 
 suspend fun convertPdfToBufferedImages(
     inputPdfFile: File,
@@ -107,14 +191,14 @@ suspend fun convertPdfToBufferedImages(
 
 
     runCatching { PDDocument.load(inputPdfFile) }
-        .onSuccess { pdDocument ->
-            pdDocument.use { document ->
+        .onSuccess {
+            it.use { document ->
                 val pdfRenderer = PDFRenderer(document)
                 val deferred = ArrayList<Deferred<Any>>()
                 for (pageIndex in 0 until document.numberOfPages) {
                     deferred += extractImage(pdfRenderer, pageIndex, onImageExtracted)
                 }
-                awaitAll(*deferred.toTypedArray())
+                deferred.awaitAll()
             }
         }
         .onFailure { e -> logger.error("Error loading:$inputPdfFile", e) }
@@ -134,3 +218,21 @@ private suspend fun extractImage(
 }
 
 typealias PageImageLoaded = (BufferedImage, pageIndex: Int) -> Unit
+
+
+fun load(file:URL): BufferedImage {
+
+    val document = Document()
+    document.setAsync(false)
+
+    document.read(file)
+    val page: Array<DjVuPage> = arrayOf<DjVuPage>(document.getPage(0, DjVuPage.MAX_PRIORITY, true))
+    val djvuImage = DjVuImage(page, true)
+    val image_local: Image = djvuImage.getImage(Canvas(), djvuImage.getPageBounds(0)).get(0)
+
+    val image = BufferedImage(image_local.getWidth(null), image_local.getHeight(null), BufferedImage.TYPE_INT_ARGB)
+    val g: Graphics = image.createGraphics()
+    g.drawImage(image_local, 0, 0, null)
+    g.dispose()
+    return image
+}
