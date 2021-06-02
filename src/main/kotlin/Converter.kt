fcd064c7aeb2ef80e3d577bf4b28899eef3c34c4
Christian Schwarz <christian.schwarz@iav.de>
2021-06-02 20:18:31 +0200

    fixed warnings

--- src/main/kotlin/Converter.kt
+++ src/main/kotlin/Converter.kt
@@ -1,37 +1,43 @@
 import com.lizardtech.djvu.DjVuPage
 import com.lizardtech.djvu.Document
 import com.lizardtech.djvubean.DjVuImage
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.withContext
 import org.apache.pdfbox.pdmodel.PDDocument
 import org.apache.pdfbox.rendering.ImageType
 import org.apache.pdfbox.rendering.PDFRenderer
 import java.awt.Canvas
 import java.awt.image.BufferedImage
 import java.nio.file.Path
-import kotlin.io.path.ExperimentalPathApi
 
-interface Converter{
+interface Converter {
     suspend fun toBufferedImages(inputFile: Path, onImageExtracted: PageImageLoaded)
 }
 
-object PdfConverter:Converter{
-    override suspend fun toBufferedImages(inputPdfFile: Path, onImageExtracted: PageImageLoaded) {
-        runCatching { PDDocument.load(inputPdfFile.toFile()) }
+object PdfConverter : Converter {
+    override suspend fun toBufferedImages(inputFile: Path, onImageExtracted: PageImageLoaded) {
+        runCatching { PDDocument.load(inputFile.toFile()) }
             .onSuccess {
                 it.use { document ->
                     val pdfRenderer = PDFRenderer(document)
-                    val deferreds = ArrayList<Deferred<Unit>>()
-                    for (pageIndex in 0 until document.numberOfPages) {
-                        deferreds += pdfPageToImage(pdfRenderer, pageIndex, onImageExtracted)
+
+                    coroutineScope {
+                        for (pageIndex in 0 until document.numberOfPages) {
+                            withContext(Dispatchers.Default) {
+                                pdfPageToImage(
+                                    pdfRenderer,
+                                    pageIndex,
+                                    onImageExtracted
+                                )
+                            }
+                        }
                     }
-                    deferreds.awaitAll()
+
 
                 }
             }
-            .onFailure { e -> logger.error("Error loading:$inputPdfFile", e) }
+            .onFailure { e -> logger.error("Error loading:$inputFile", e) }
 
     }
 
@@ -39,7 +45,7 @@ object PdfConverter:Converter{
         pdfRenderer: PDFRenderer,
         pageIndex: Int,
         onImageExtracted: PageImageLoaded
-    ): Deferred<Unit> = GlobalScope.async {
+    ) {
         runCatching { pdfRenderer.renderImageWithDPI(pageIndex, 120f, ImageType.GRAY) }
             .onSuccess { pageImage -> onImageExtracted(pageImage, pageIndex) }
             .onFailure { e -> logger.error("Error extracting PDF Document pageIndex $pageIndex", e) }
@@ -48,7 +54,7 @@ object PdfConverter:Converter{
 
 }
 
-object DjvuConverter:Converter {
+object DjvuConverter : Converter {
 
 
     override suspend fun toBufferedImages(inputFile: Path, onImageExtracted: PageImageLoaded) {
@@ -61,39 +67,44 @@ object DjvuConverter:Converter {
                 return@toBufferedImages
             }
 
-        val deferreds = ArrayList<Deferred<Unit>>()
-        for (pageNumber in 0 until document.size()) {
-            deferreds += pageToImage(document, pageNumber, onImageExtracted)
+        coroutineScope {
+            for (pageNumber in 0 until document.size()) {
+                withContext(Dispatchers.Default) {
+                    pageToImageAsync(
+                        document,
+                        pageNumber,
+                        onImageExtracted
+                    )
+                }
+            }
         }
-        deferreds.awaitAll()
-
     }
 
-    private fun pageToImage(
+    private fun pageToImageAsync(
         document: Document,
         pageIndex: Int,
         onImageExtracted: PageImageLoaded
-    ): Deferred<Unit> =
-        GlobalScope.async {
+    ) {
 
-            runCatching { document.getPage(pageIndex, DjVuPage.MAX_PRIORITY, true) }
-                .onSuccess { page ->
-                    val pages = arrayOf<DjVuPage>(page)
-                    val djvuImage = DjVuImage(pages, true)
-                    val image = djvuImage.getImage(Canvas(), djvuImage.getPageBounds(0))[0]
 
-                    val width = image.getWidth(null)
-                    val height = image.getHeight(null)
+        runCatching { document.getPage(pageIndex, DjVuPage.MAX_PRIORITY, true) }
+            .onSuccess { page ->
+                val pages = arrayOf<DjVuPage>(page)
+                val djvuImage = DjVuImage(pages, true)
+                val image = djvuImage.getImage(Canvas(), djvuImage.getPageBounds(0))[0]
 
-                    val bufferedImage = BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY)
+                val width = image.getWidth(null)
+                val height = image.getHeight(null)
 
-                    val g = bufferedImage.createGraphics()
-                    g.drawImage(image, 0, 0, null)
-                    g.dispose()
+                val bufferedImage = BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY)
 
-                    onImageExtracted(bufferedImage, pageIndex)
-                }.onFailure { e -> logger.error("", e) }
+                val g = bufferedImage.createGraphics()
+                g.drawImage(image, 0, 0, null)
+                g.dispose()
 
-        }
+                onImageExtracted(bufferedImage, pageIndex)
+            }.onFailure { e -> logger.error("", e) }
+
+    }
 
 }
